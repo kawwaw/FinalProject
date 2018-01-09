0ef90e10667c32aca775221ad6040eb731f49343
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2018-01-09 15:37:53 +0100

    initial loading of data

--- js/plots.js
+++ js/plots.js
@@ -1,3 +1,11 @@
+d3.select(window).on('load', init);
 
+function init() {
+  d3.tsv('HFUDD20.txt', function(error, data) {
+    if (error) {
+      throw error;
+    }
 
-console.log("final project");
+    console.log(data[0]);
+  });
+}
