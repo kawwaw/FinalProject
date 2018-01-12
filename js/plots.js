13fe3cde4477c3ad6e46113ad430e4fd6a62b889
kawwaw <fsc442@alumni.ku.dk>
2018-01-12 13:32:32 +0100

    mapDK appended

--- js/plots.js
+++ js/plots.js
@@ -1,11 +1,49 @@
 d3.select(window).on('load', init);
 
 function init() {
-  d3.tsv('HFUDD20.txt', function(error, data) {
-    if (error) {
-      throw error;
-    }
+    var mapsvg = d3.select('#mapsvg');
+    var mapW = +mapsvg.node().getBoundingClientRect().width;
+    var mapH = +mapsvg.node().getBoundingClientRect().height;
 
-    console.log(data[0]);
-  });
+    var Mercator = d3.geoMercator()
+        .translate([ mapW / 2, mapH / 2 ])
+        //.scale(150000)
+        .scale(1400)
+        //.center([  10.21076, 56.15674 ]); //Århus
+        .center([  10.21076, 56.2 ]); //Århus
+
+    //create path variable
+    var path = d3.geoPath()
+        .projection(Mercator);
+
+    var offsetMercator = Mercator.translate();
+
+
+    d3.queue()
+      .defer(d3.json, 'regioner.geojson')
+      .defer(d3.tsv, 'HFUDD20.txt')
+      .await(ready);
+
+    function ready(error, regions, data) {
+        if (error) {
+            throw error;
+        }
+        console.log("test ny");
+        console.log(data[0]);
+        console.log(regions);
+
+
+        mapsvg.selectAll("path")
+            .data(regions.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", "lightgrey")
+            .style("stroke", "grey")
+            .style("stroke-width", 0.5);
+
+
+    }
 }
+
+
