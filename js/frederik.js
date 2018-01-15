e44f0ae912ee90cb4fab3959d645dbd92d4becac
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2018-01-15 11:36:35 +0100

    added fullpage.js

---
+++ js/frederik.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+    //Navigation
+		menu: '#menu',
+		lockAnchors: false,
+		anchors:['firstPage', 'secondPage'],
+		navigation: false,
+		navigationPosition: 'right',
+		navigationTooltips: ['firstSlide', 'secondSlide'],
+		showActiveTooltip: false,
+		slidesNavigation: false,
+		slidesNavPosition: 'bottom',
+  
+  });
+});
+
+
