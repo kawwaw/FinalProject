7f1a436a17f7f17716ce4b39a27b494a0d80c383
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2018-01-17 17:06:52 +0100

    initial data mapping

--- js/frederik.js
+++ js/frederik.js
@@ -34,8 +34,12 @@ function frederik() {
                   .attr('width', w)
                   .attr('height', h);
 
-    d3.json('regioner.geojson', function(error, data) {
+    d3.queue()
+        .defer(d3.json, 'data/regioner.geojson')
+        .defer(d3.tsv, 'data/HFUDD20years29time2017highestEDaggregated.txt')
+        .await(ready);
 
+    function ready(error, geodata, data) {
         if (error) throw error;
         var regionColors = {
             1081: 'red',
@@ -45,22 +49,54 @@ function frederik() {
             1085: 'magenta',
         };
 
+        console.log(data);
+
+
+        var preparedData = data.map(function(d) {
+            return {
+                levels: {
+                    'H10': d[Object.keys(d)[15]],
+                    'H20': d[Object.keys(d)[16]],
+                    'H30': d[Object.keys(d)[17]],
+                    'H40': d[Object.keys(d)[18]],
+                    'H50': d[Object.keys(d)[19]],
+                    'H60': d[Object.keys(d)[20]],
+                    'H70': d[Object.keys(d)[21]],
+                    'H80': d[Object.keys(d)[22]],
+                    'H90': d[Object.keys(d)[23]],
+                },
+                    region: d.region,
+                    gender: d.sex,
+            }
+        });
+
+
 
         // set the selected menu item to selectedLevel
-        var selectedLevel = '';
+        var selectedLevel = 'H10';
+        var selectedGender = 'Men';
         $('#level-menu li').click(function() {
             // remove classes from all
             $('#level-menu li').removeClass('active');
             // add class to the one we clicked
             $(this).addClass('active');
             selectedLevel = $(this).find('a').data('value');
-            console.log(selectedLevel);
         });
 
+        // transforming the data to be region->value
+        var filteredByGender = preparedData.filter(function(d) {
+                                            return d.gender === selectedGender;
+                                        });
+
+        var obj = {};
+        for(var i = 0; i < filteredByGender.length; i++){
+            obj[filteredByGender[i].region] = filteredByGender[i].levels[selectedLevel];
+        }
+        console.log(obj);
 
         svg.append('g')
             .selectAll('path')
-            .data(data.features)
+            .data(geodata.features)
             .enter()
             .append('path')
             .style('fill', function(d) {
@@ -69,5 +105,5 @@ function frederik() {
             .style('stroke', 'black')
             .style('stroke-width', 1)
             .attr('d', path);
-    })
+    }
 }
