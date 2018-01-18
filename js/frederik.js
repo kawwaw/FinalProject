aa34c68bc1cbd4587ed14c6cbeffb2e3451add87
Frederik Bøgelund Larsen <frederikblarsen@gmail.com>
2018-01-18 18:27:28 +0100

    map implemented

--- js/frederik.js
+++ js/frederik.js
@@ -41,16 +41,14 @@ function frederik() {
 
     function ready(error, geodata, data) {
         if (error) throw error;
-        var regionColors = {
-            1081: 'red',
-            1082: 'green',
-            1083: 'blue',
-            1084: 'orange',
-            1085: 'magenta',
-        };
 
         console.log(data);
 
+        var genders = {
+            'M': 'Men',
+            'W': 'Women',
+            'A': 'Men and women'
+        };
 
         var preparedData = data.map(function(d) {
             return {
@@ -58,12 +56,13 @@ function frederik() {
                     'H10': d[Object.keys(d)[15]],
                     'H20': d[Object.keys(d)[16]],
                     'H30': d[Object.keys(d)[17]],
-                    'H40': d[Object.keys(d)[18]],
-                    'H50': d[Object.keys(d)[19]],
-                    'H60': d[Object.keys(d)[20]],
-                    'H70': d[Object.keys(d)[21]],
-                    'H80': d[Object.keys(d)[22]],
-                    'H90': d[Object.keys(d)[23]],
+                    'H35': d[Object.keys(d)[18]],
+                    'H40': d[Object.keys(d)[19]],
+                    'H50': d[Object.keys(d)[20]],
+                    'H60': d[Object.keys(d)[21]],
+                    'H70': d[Object.keys(d)[22]],
+                    'H80': d[Object.keys(d)[23]],
+                    'H90': d[Object.keys(d)[24]],
                 },
                     region: d.region,
                     gender: d.sex,
@@ -71,36 +70,71 @@ function frederik() {
         });
 
 
+        function filterByLevelAndGender(level, gender) {
+            var filteredByGender = preparedData.filter(function(d) {
+                return d.gender === genders[gender];
+            });
 
-        // set the selected menu item to selectedLevel
+            var obj = {};
+            for (var i = 0; i < filteredByGender.length; i++) {
+                obj[filteredByGender[i].region] = filteredByGender[i].levels[level];
+            }
+
+            return obj;
+        }
+        function chooseColor(value) {
+            if (value <= 900) {
+                return 1
+            } else if (value > 900 && value < 1000) {
+                return 2;
+            } else if (value > 1000 && value <= 2000) {
+                return 3;
+            } else {
+                return 4;
+            }
+        }
+        var color = d3.scaleThreshold()
+                        .domain(d3.range(0, 10))
+                        .range(d3.schemeReds[9]);
         var selectedLevel = 'H10';
-        var selectedGender = 'Men';
+        var selectedGender = 'A';
+
         $('#level-menu li').click(function() {
-            // remove classes from all
             $('#level-menu li').removeClass('active');
-            // add class to the one we clicked
             $(this).addClass('active');
             selectedLevel = $(this).find('a').data('value');
+            var regionToValue = filterByLevelAndGender(selectedLevel, selectedGender);
+            console.log(regionToValue);
+            svg.selectAll('path')
+                .transition()
+                .style('fill', function(d) {
+                    return color(regionToValue[d.properties.REGIONNAVN] / 700);
+                });
         });
 
-        // transforming the data to be region->value
-        var filteredByGender = preparedData.filter(function(d) {
-                                            return d.gender === selectedGender;
-                                        });
+        $('#gender-menu li').click(function() {
+            $('#gender-menu li').removeClass('active');
+            $(this).addClass('active');
+            selectedGender = $(this).find('a').data('value');
+            var regionToValue = filterByLevelAndGender(selectedLevel, selectedGender);
+            console.log(regionToValue);
+            svg.selectAll('path')
+                .transition()
+                .style('fill', function(d) {
+                    return color(regionToValue[d.properties.REGIONNAVN] / 700);
+                });
+        });
 
-        var obj = {};
-        for(var i = 0; i < filteredByGender.length; i++){
-            obj[filteredByGender[i].region] = filteredByGender[i].levels[selectedLevel];
-        }
-        console.log(obj);
+        var regionToValue = filterByLevelAndGender('H10', 'A');
 
+        console.log('obj', filterByLevelAndGender('H20', 'W'));
         svg.append('g')
             .selectAll('path')
             .data(geodata.features)
             .enter()
             .append('path')
             .style('fill', function(d) {
-                return regionColors[d.properties.REGIONKODE];
+                return color(regionToValue[d.properties.REGIONNAVN] / 700);
             })
             .style('stroke', 'black')
             .style('stroke-width', 1)
